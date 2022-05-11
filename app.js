2b230e7776408263517b734d7eb841ceab62c07d
Michael <michaelthai16@gmail.com>
2022-05-11 18:11:56 -0500

    created first user prompt

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
 
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 // console.log(profileDataArgs);
 
